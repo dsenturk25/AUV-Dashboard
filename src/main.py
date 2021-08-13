b44f0481cd3b194fbd22fc3c399be320d7982655
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2021-08-13 21:41:55 +0300

    Improvements on the dashboard

--- src/main.py
+++ src/main.py
@@ -1,15 +1,74 @@
+
+import sys
+import os
 import cv2
 import tkinter as tk
 import tkinter.font as font
 from PIL import Image
 from PIL import ImageTk
+from tkinter import messagebox
 import numpy as np
 
 root = tk.Tk()
+
 root.resizable(0, 0)
+root.title("Tigers Robotics ROV Dashboard")
 
 canvas = tk.Canvas(root, width=1000, height=700)
 canvas.pack()
+
+def connect(serverIp):
+
+    cap = cv2.VideoCapture(str(serverIp))
+    while True:
+        ret, frame = cap.read()
+
+        if ret is not True:
+            messagebox.showwarning("Warning", "Cannot connect to the server")
+            break
+
+        hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        l_b = np.array([lower_hue.get(), lower_saturation.get(), lower_value.get()])
+        u_b = np.array([upper_hue.get(), upper_saturation.get(), upper_value.get()])
+
+        mask = cv2.inRange(hsv_image, l_b, u_b)
+
+        final = cv2.bitwise_and(frame, frame, mask=mask)
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        final = cv2.cvtColor(final, cv2.COLOR_BGR2RGB)
+
+        finalGrayContours = cv2.cvtColor(final, cv2.COLOR_RGB2GRAY)
+
+        _, finalThresh = cv2.threshold(finalGrayContours, 0, 255, cv2.THRESH_BINARY)
+
+        contours, hierarchy = cv2.findContours(finalThresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if cv2.contourArea(contour) < 500:
+                continue
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2, cv2.LINE_AA)
+            cv2.rectangle(final, (x, y), (x + w, y + h), (255, 255, 0), 2, cv2.LINE_AA)
+
+        frame = Image.fromarray(frame)
+        final = Image.fromarray(final)
+
+        frame = ImageTk.PhotoImage(frame)
+        final = ImageTk.PhotoImage(final)
+
+        original_feed.image = frame
+        original_feed.configure(image=frame)
+
+        filtered_feed.image = final
+        filtered_feed.configure(image=final)
+
+        root.update()
+    cap.release()
 
 header = tk.Label(canvas, text="Tigers Robotics ROV Dashboard", font=("Arial", 20))
 header.place(relwidth=0.35, relheight=0.1, rely=0.02, relx=0.02)
@@ -32,6 +91,8 @@ lower_value_label.place(relwidth=0.1, relheight=0.1, rely=0.32, relx=0.04)
 lower_value = tk.Scale(canvas, from_=0, to=255, orient="horizontal")
 lower_value.place(relwidth=0.35, relheight=0.1, rely=0.33, relx=0.13)
 
+# - - - - - - - - - - - - - - -
+
 upper_hue_label = tk.Label(canvas, text="Upper Hue", font=("Arial", 10))
 upper_hue_label.place(relwidth=0.1, relheight=0.1, rely=0.44, relx=0.04)
 
@@ -52,55 +113,27 @@ upper_value.place(relwidth=0.35, relheight=0.1, rely=0.65, relx=0.13)
 
 # connect to server UI widgets
 
-server_label = tk.Label(canvas, text="Server (localhost:3000, RPI 4)", font=("Arial", 16))
-server_label.place(relwidth=0.22, relheight=0.1, rely=0.80, relx=0.1)
+server_entry_label = tk.Label(canvas, text="Server IP")
+server_entry_label.place(relwidth=0.1, relheight=0.05, rely=0.825, relx=0.05)
 
-connect_button = tk.Button(canvas, text="Connect")
-connect_button.place(relwidth=0.1, relheight=0.05, rely=0.825, relx=0.34)
+server_entry = tk.Entry(canvas, font=("Arial", 16), borderwidth=2, relief="groove")
+server_entry.place(relwidth=0.22, relheight=0.05, rely=0.825, relx=0.15)
+
+connect_button = tk.Button(canvas, text="Connect", command=lambda:connect(server_entry.get()))
+connect_button.place(relwidth=0.1, relheight=0.05, rely=0.825, relx=0.38)
 
 # imageViews for original and filtered
 
-cap = cv2.VideoCapture('http:/localhost:3000/video')
+original_feed_label = tk.Label(canvas, text="Original feed")
+original_feed_label.place(relwidth=0.4, relheight=0.05, rely=0.05, relx=0.5)
 
 original_feed = tk.Label(canvas)
 original_feed.place(relwidth=0.4, relheight=0.4, rely=0.1, relx=0.5)
 
+filtered_feed_label = tk.Label(canvas, text="Filtered feed")
+filtered_feed_label.place(relwidth=0.4, relheight=0.05, rely=0.5, relx=0.5)
+
 filtered_feed = tk.Label(canvas)
-filtered_feed.place(relwidth=0.4, relheight=0.4, rely=0.5, relx=0.5)
-
-while True:
-    ret, frame = cap.read()
-
-    hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    l_b = np.array([lower_hue.get(), lower_saturation.get(), lower_value.get()])
-    u_b = np.array([upper_hue.get(), upper_saturation.get(), upper_value.get()])
-
-    mask = cv2.inRange(hsv_image, l_b, u_b)
-    final = cv2.bitwise_and(frame, frame, mask=mask)
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    final = cv2.cvtColor(final, cv2.COLOR_BGR2RGB)
-
-    frame = Image.fromarray(frame)
-    final = Image.fromarray(final)
-
-    frame = ImageTk.PhotoImage(frame)
-    final = ImageTk.PhotoImage(final)
-
-    original_feed.image = frame
-    original_feed.configure(image=frame)
-    filtered_feed.image = final
-    filtered_feed.configure(image=final)
-
-    root.update()
-
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-
+filtered_feed.place(relwidth=0.4, relheight=0.4, rely=0.55, relx=0.5)
 
 root.mainloop()
-
-cap.release()
-cv2.destroyAllWindows()
